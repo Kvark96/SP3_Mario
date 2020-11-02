5a99bd372f468edfadfe699e682bdd3276abf95a
Kvark96 <47635241+Kvark96@users.noreply.github.com>
2020-11-02 15:11:11 +0100

    Proof of concept maps

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Map<String, Integer> soldPizzas = new HashMap<>();
+        addPizza(soldPizzas, "pepperoni");
+        addPizza(soldPizzas, "hawaii");
+        addPizza(soldPizzas, "pepperoni");
+        System.out.println(soldPizzas);
+    }
+
+    /*
+    * map <type, type>
+    * {key, value}
+    * map.get(key) -> value
+    * map.put(key,value) -> {key, value}
+    * */
+
+    static void addPizza(Map<String,Integer> map, String pizza){
+        if(map.containsKey(pizza)){
+            map.put(pizza, map.get(pizza) + 1);
+        } else {
+            map.put(pizza, 1);
+        }
     }
 }
