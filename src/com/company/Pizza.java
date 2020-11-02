5df732e798c8f1de89f46ed6db88e21806707b8a
Hejlesen <70164440+HejlesenCPH@users.noreply.github.com>
2020-11-02 15:54:44 +0100

    Meny and Pizza class added

---
+++ src/com/company/Pizza.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Pizza {
+
+    Double price;
+    String Name;
+
+    public Pizza(Double price, String name) {
+        this.price = price;
+        Name = name;
+    }
+
+
+
+}
