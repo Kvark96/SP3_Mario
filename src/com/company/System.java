70eba0dec28a16f57028a3ea9735ccdac8835de0
Stobes <71084144+Stobes@users.noreply.github.com>
2020-11-05 13:08:29 +0100

    Created Syustems

---
+++ src/com/company/System.java
@@ -0,0 +1,32 @@
+package com.company;
+// Written by Tobias Linge 05-11-2020.
+
+import java.util.ArrayList;
+
+
+public class Systems
+{
+    ArrayList<Order> orderArrayList;
+    Statistics statistic;
+
+
+    public Systems() {
+        orderArrayList = new ArrayList<Order>();
+        this.statistic = new Statistics();
+    }
+
+    public void addOrder(Order order)
+    {
+        orderArrayList.add(order);
+    }
+
+    public void removeOrder(int i)
+    {
+        orderArrayList.remove(i);
+    }
+
+  /*  public void addPizza()
+    {
+
+   */ }
+}
