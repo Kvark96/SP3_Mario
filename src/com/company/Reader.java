787f1f775cb5db5749d1045d584126498c850993
Hejlesen <70164440+HejlesenCPH@users.noreply.github.com>
2020-11-05 10:33:21 +0100

    Reader updates with Add ons

--- src/com/company/Reader.java
+++ src/com/company/Reader.java
@@ -6,10 +6,8 @@ import java.util.Scanner;
 /// shit work :)
 
 public class Reader {
-    public static void main(String[] args) throws IOException {
-        // open file input stream
-        BufferedReader reader = new BufferedReader(new FileReader(
-                "Pizzaer.csv"));
+    public static void printPizzaMenu () throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("Pizzaer.csv"));
         // read file line by line
         String line = null;
         Scanner scanner = null;
@@ -22,23 +20,43 @@ public class Reader {
             System.out.println(line);
         }
     }
-}
-        /*
-        String filename = "Pizzaer.csv";
-        File file = new File(filename);
-        System.out.println(file.length());
-        try {
-            Scanner input = new Scanner(file);
-            while (input.hasNext()) {
-                String data = input.next();
-                System.out.println(data);
-            }
-            input.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+    public static void printAddons () throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("Addons.csv"));
+        // read file line by line
+        String line = null;
+        Scanner scanner = null;
+        int index = 0;
+
+
+        while ((line = reader.readLine()) != null) {
+
+            scanner = new Scanner(line);
+            System.out.println(line);
         }
     }
 
 
-         */
+
+
+
+}
+
+//        String filename = "Pizzaer.csv";
+//        File file = new File(filename);
+//        System.out.println(file.length());
+//        try {
+//            Scanner input = new Scanner(file);
+//            while (input.hasNext()) {
+//                String data = input.next();
+//                System.out.println(data);
+//            }
+//            input.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
 
