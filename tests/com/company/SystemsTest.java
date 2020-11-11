6bded7e2d922cfbba0780cf87e0a59ff010945d9
Stobes <71084144+Stobes@users.noreply.github.com>
2020-11-11 12:20:58 +0100

    Update SystemsTest.java

--- tests/com/company/SystemsTest.java
+++ tests/com/company/SystemsTest.java
@@ -2,9 +2,11 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class SystemsTest {
@@ -12,6 +14,8 @@ public class SystemsTest {
     Order order1 = new Order(lst, 1700);
     Order order2 = new Order(lst, 1800);
     ArrayList<Order> orderList = new ArrayList<>();
+    File A = new File("src/Addons.csv");
+    File P = new File("src/Pizzaer.csv");
 
     @Test
     void addOrderTest()
@@ -31,7 +35,15 @@ public class SystemsTest {
         //Removes orders and updates correctly
         orderList.remove(0);
         assertEquals(0, orderList.indexOf(order2));
+
     }
 
-
+    @Test
+    void setupTest()
+    {
+        assertTrue(P.exists());
+        assertTrue(P.length()>0);
+        assertTrue(A.exists());
+        assertTrue(A.length()>0);
+    }
 }
